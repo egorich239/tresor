85f67805cc346e27808cd90db833477be53b2ba9
Ivan Egorov <me@egori.ch>
2025-08-07 10:39:29 +0200

    AES api redesign, dysfunc

--- src/enc.rs
+++ src/enc.rs
@@ -1,5 +1,6 @@
-use crate::api::error::ApiResult;
 use crate::api::session::SessionEncKey;
+use crate::api::session::SessionId;
+use aes_gcm::aead::Payload;
 use aes_gcm::{
     Aes256Gcm, Nonce,
     aead::{Aead, KeyInit},
@@ -7,19 +8,101 @@ use aes_gcm::{
 use rand::RngCore;
 use sha2::digest::consts::U12;
 
-pub fn encrypt(cleartext: &[u8], key: &SessionEncKey) -> ApiResult<(Vec<u8>, Nonce<U12>)> {
-    let cipher = Aes256Gcm::new_from_slice(key.as_slice()).unwrap();
-
-    let mut nonce_bytes = [0u8; 12];
-    rand::thread_rng().fill_bytes(&mut nonce_bytes);
-    let nonce = Nonce::from_slice(&nonce_bytes);
-
-    let ciphertext = cipher.encrypt(nonce, cleartext).unwrap();
-
-    Ok((ciphertext, *nonce))
+pub struct AesSession {
+    enc_key: SessionEncKey,
+    session_id: SessionId,
 }
 
-pub fn decrypt(ciphertext: &[u8], nonce: &Nonce<U12>, key: &SessionEncKey) -> Option<Vec<u8>> {
-    let cipher = Aes256Gcm::new_from_slice(key.as_slice()).unwrap();
-    cipher.decrypt(nonce, ciphertext).ok()
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
+pub struct AesNonce(Nonce<U12>);
+pub struct AesCiphertextSend(Vec<u8>);
+pub struct AesCiphertextRecv<'n, 'c> {
+    nonce: &'n [u8],
+    ciphertext: &'c [u8],
+}
+
+impl AesSession {
+    pub fn new(enc_key: SessionEncKey, session_id: SessionId) -> Self {
+        Self {
+            enc_key,
+            session_id,
+        }
+    }
+
+    pub fn encrypt(&self, cleartext: &[u8]) -> AesCiphertextSend {
+        let cipher = Aes256Gcm::new_from_slice(self.enc_key.as_slice()).unwrap();
+
+        let mut nonce_bytes = [0u8; 12];
+        rand::thread_rng().fill_bytes(&mut nonce_bytes);
+        let nonce = Nonce::from_slice(&nonce_bytes);
+
+        let ciphertext = cipher
+            .encrypt(
+                nonce,
+                Payload {
+                    msg: cleartext,
+                    aad: self.session_id.as_slice(),
+                },
+            )
+            .unwrap();
+
+        let mut full_text = Vec::new();
+        full_text.extend_from_slice(&nonce);
+        full_text.extend_from_slice(&ciphertext);
+        AesCiphertextSend(full_text)
+    }
+
+    pub fn decrypt(&self, ciphertext: AesCiphertextRecv) -> Option<Vec<u8>> {
+        let cipher = Aes256Gcm::new_from_slice(self.enc_key.as_slice()).unwrap();
+        let nonce = Nonce::from_slice(&ciphertext.nonce);
+        cipher
+            .decrypt(
+                nonce,
+                Payload {
+                    msg: &ciphertext.ciphertext,
+                    aad: self.session_id.as_slice(),
+                },
+            )
+            .ok()
+    }
+}
+
+impl From<AesCiphertextSend> for Vec<u8> {
+    fn from(ciphertext: AesCiphertextSend) -> Self {
+        ciphertext.0
+    }
+}
+
+impl<'n, 'c> AesCiphertextRecv<'n, 'c> {
+    pub fn nonce(&self) -> AesNonce {
+        AesNonce(Nonce::from_slice(self.nonce).clone())
+    }
+}
+
+impl<'c> TryFrom<&'c [u8]> for AesCiphertextRecv<'c, 'c> {
+    type Error = ();
+
+    fn try_from(value: &'c [u8]) -> Result<Self, Self::Error> {
+        if value.len() < 12 {
+            return Err(());
+        }
+        Ok(AesCiphertextRecv {
+            nonce: &value[..12],
+            ciphertext: &value[12..],
+        })
+    }
+}
+
+impl<'n, 'c> TryFrom<(&'n [u8], &'c [u8])> for AesCiphertextRecv<'n, 'c> {
+    type Error = ();
+
+    fn try_from(value: (&'n [u8], &'c [u8])) -> Result<Self, Self::Error> {
+        if value.0.len() != 12 {
+            return Err(());
+        }
+        Ok(AesCiphertextRecv {
+            nonce: value.0,
+            ciphertext: value.1,
+        })
+    }
 }
